f4641e45b78ef6b8be291be1a265c2e2ba1d679e
1596944197 <1596944197@qq.com>
2023-04-06 17:31:12 +0800

    feat:二稿完成

--- index.mjs
+++ index.mjs
@@ -6,7 +6,7 @@ const server = createServer((req, res) => {
   // v1.0.0
   // createReadStream('./t.txt', { encoding: 'utf8', highWaterMark: 1 * 100 }).pipe(res);
   // v2.0.0
-  createReadStream('./t.md', { encoding: 'utf8', highWaterMark: 10 }).pipe(res);
+  createReadStream('./t.md', { encoding: 'utf8', }).pipe(res);
 });
 
 server.listen(3002, () => console.log('is ok'));
